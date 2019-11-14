c2c65f223d61c616dab12e8a5972e47e6d4a9479
Waldemar Giss <w.giss@gmx.de>
2019-11-14 13:32:49 +0100

    Update config.inc.php
    Datenbank name geändert

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -3,7 +3,7 @@
 $db_host = 'localhost';
 $db_user = 'root';
 $db_password = '';
-$db_name = 'hi';
+$db_name = 'test';
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
