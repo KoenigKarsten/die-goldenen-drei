329b849e556b69f0ee8016cd65c65af38b59dbfd
Waldemar Giss <w.giss@gmx.de>
2019-11-18 09:30:23 +0100

    Update SQLDAOFactory.php

--- mapper/SQLDAOFactory.php
+++ mapper/SQLDAOFactory.php
@@ -4,12 +4,12 @@
 
 class SQLDAOFactory{
 	private static $instance;
-
+	
 	public static function getInstance(){
 		include_once ("./inc/config.inc.php");
 		if(!self::$instance){
 			try{
-				self::$instance = new \mysqli('localhost', 'root', '', 'hi');
+				self::$instance = new \mysqli('192.168.101.163', 'admin', '1234', 'test');
 				self::$instance->set_charset("utf8");
 			}
 			catch(Exception $e){
