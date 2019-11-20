f078457ed384dda850a4cf8240ef629d419d69b5
Waldemar Giss <w.giss@gmx.de>
2019-11-20 10:16:24 +0100

    refresh nach reiter

--- reservierung.php
+++ reservierung.php
@@ -5,66 +5,66 @@ require_once("inc/functions.php");
 
 include("templates/header.php")
 ?>
-    <div class="container main-container registration-form">
+<div class="container main-container registration-form">
     <h1>Reservierung</h1>
-<?php
-$showFormular = true; //Variable ob das Reservierungsformular anezeigt werden soll
+    <?php
+    $showFormular = true; //Variable ob das Reservierungsformular anezeigt werden soll
 
-if (isset($_GET['register'])) {
-    $error = false;
-    $vorname = trim($_POST['vorname']);
-    $nachname = trim($_POST['nachname']);
-    $email = trim($_POST['email']);
-    $passwort = $_POST['passwort'];
-    $passwort2 = $_POST['passwort2'];
+    if (isset($_GET['register'])) {
+        $error = false;
+        $vorname = trim($_POST['vorname']);
+        $nachname = trim($_POST['nachname']);
+        $email = trim($_POST['email']);
+        $passwort = $_POST['passwort'];
+        $passwort2 = $_POST['passwort2'];
 
-    if (empty($vorname) || empty($nachname) || empty($email)) {
-        echo 'Bitte alle Felder ausfüllen<br>';
-        $error = true;
-    }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
-        $error = true;
-    }
-    if (strlen($passwort) == 0) {
-        echo 'Bitte ein Passwort angeben<br>';
-        $error = true;
-    }
-    if ($passwort != $passwort2) {
-        echo 'Die Passwörter müssen übereinstimmen<br>';
-        $error = true;
-    }
-
-    //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
-    if (!$error) {
-        $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-        $result = $statement->execute(array('email' => $email));
-        $user = $statement->fetch();
-
-        if ($user !== false) {
-            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+        if (empty($vorname) || empty($nachname) || empty($email)) {
+            echo 'Bitte alle Felder ausfüllen<br>';
             $error = true;
         }
-    }
 
-    //Keine Fehler, wir können den Nutzer registrieren
-    if (!$error) {
-        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+            $error = true;
+        }
+        if (strlen($passwort) == 0) {
+            echo 'Bitte ein Passwort angeben<br>';
+            $error = true;
+        }
+        if ($passwort != $passwort2) {
+            echo 'Die Passwörter müssen übereinstimmen<br>';
+            $error = true;
+        }
 
-        $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
-        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
+        //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+        if (!$error) {
+            $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+            $result = $statement->execute(array('email' => $email));
+            $user = $statement->fetch();
 
-        if ($result) {
-            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
-            $showFormular = false;
-        } else {
-            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+            if ($user !== false) {
+                echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+                $error = true;
+            }
+        }
+
+        //Keine Fehler, wir können den Nutzer registrieren
+        if (!$error) {
+            $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+
+            $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
+            $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
+
+            if ($result) {
+                echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+                $showFormular = false;
+            } else {
+                echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+            }
         }
     }
-}
 
-if ($showFormular) {
+    //if ($showFormular) {
     ?>
 
     <form action="?register=1" method="post">
@@ -99,9 +99,9 @@ if ($showFormular) {
         </div>
         <button type="submit" class="btn btn-lg btn-primary btn-block">Reservieren</button>
     </form>
-    </div>
+</div>
 
 
-    <?php
-    include("templates/footer.php")
-    ?>
+<?php
+include("templates/footer.php")
+?>
