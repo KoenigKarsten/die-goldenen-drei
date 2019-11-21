14b029164d49966c6cc27322cf45d201b87bea5d
Karsten König <KKoenig@it-ab.de>
2019-11-21 14:45:38 +0100

    Änderung
    Leider bin ich noch nicht fertig

--- gaesteueberblick.php
+++ gaesteueberblick.php
@@ -1,16 +1,33 @@
 <?php
 
-
 session_start();
 require_once("inc/config.php.inc");
 require_once("inc/functions.php");
 
-include("templates/header.php")
+include("templates/header.php");
+
+spl_autoload_register();
+use mapper\GastDAO;
+use model\Gast;
+use mapper\SQLDAOFactory;
+
 ?>
     <div class="container main-container registration-form">
         <h1>Gäste</h1>
         <?php
-        $showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+        // $showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+
+        $anrede = "";
+        $vorname = "";
+        $nachname = "";
+        $strasse = "";
+        $hausnr = "";
+        $zusatz = "";
+        $plz = "";
+        $ort = "";
+        $land = "";
+        $telefon = "";
+        $email = "";
 
         if (isset($_POST['submit'])) {
             $anrede = $_POST['anrede'];
@@ -22,57 +39,69 @@ include("templates/header.php")
             $ort = $_POST['ort'];
             $land = $_POST['land'];
             $zusatz = $_POST['zusatz'];
-            $telefon = $_POST['telefonNr'];
-            $email = $_POST['emailAddy'];
+            $telefon = $_POST['telefon'];
+            $email = $_POST['email'];
+
+            $gast = new Gast($anrede, $vorname, $nachname, $strasse, $hausnr, $zusatz, $plz, $ort, $land, $telefon, $email);
+            $gastDao = new GastDAO();
+
+            $gastDao->create($gast);
+            $gastDao->read($gast);
 
             if (empty($vorname) || empty($nachname) || empty($email)) {
                 echo 'Bitte alle Felder ausfüllen<br>';
                 $error = true;
             }
+    
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+                $error = true;
+            }
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
-            $error = true;
+        } else {
+            echo "Fehler";
         }
-        if (strlen($passwort) == 0) {
-            echo 'Bitte ein Passwort angeben<br>';
-            $error = true;
-        }
-        if ($passwort != $passwort2) {
-            echo 'Die Passwörter müssen übereinstimmen<br>';
-            $error = true;
-        }
+        
+
+        // if (strlen($passwort) == 0) {
+        //     echo 'Bitte ein Passwort angeben<br>';
+        //     $error = true;
+        // }
+        // if ($passwort != $passwort2) {
+        //     echo 'Die Passwörter müssen übereinstimmen<br>';
+        //     $error = true;
+        // }
 
             //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
-            if (!$error) {
-                $statement = $pdo->prepare("SELECT * FROM gast WHERE email = :email");
-                $result = $statement->execute(array('email' => $email));
-                $gast = $statement->fetch();
+            // if (!$error) {
+            //     $statement = $pdo->prepare("SELECT * FROM gast WHERE email = :email");
+            //     $result = $statement->execute(array('email' => $email));
+            //     $gast = $statement->fetch();
 
-                if ($gast !== true) {
-                    echo 'Diese E-Mail-Adresse ist nicht vergeben<br>';
-                    $error = false;
-                }
-            }
-        }
+            //     if ($gast !== true) {
+            //         echo 'Diese E-Mail-Adresse ist nicht vergeben<br>';
+            //         $error = false;
+            //     }
+            // }
+        
 
-        //Keine Fehler, wir können den Nutzer registrieren
-        if (!$error) {
-            $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+        // //Keine Fehler, wir können den Nutzer registrieren
+        // if (!$error) {
+        //     $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
 
-            $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
-            $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
+        //     $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
+        //     $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
 
-            if ($result) {
-                echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
-                $showFormular = false;
-            } else {
-                echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
-            }
-        }
+        //     if ($result) {
+        //         echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+        //         $showFormular = false;
+        //     } else {
+        //         echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+        //     }
+        // }
     
 
-    if ($showFormular) {
+    
         ?>
 
         <form action="?register=2" method="post">
@@ -115,7 +144,7 @@ include("templates/header.php")
                         
                 <div class="form-group">
                     <label for="inputHausNr">Hausnummer:</label>
-                    <input type="text" id="inputHausNr" size="5" maxlength="10" name="hausnr"
+                    <input type="text" id="inputHausNr" size="5" maxlength="10" name="hausnummer"
                          class="form-control">
                 </div>
                         
@@ -127,7 +156,7 @@ include("templates/header.php")
                         
                 <div class="form-group">
                     <label for="inputPLZ">PLZ:</label>
-                    <input type="text" id="inputPLZ" size="5" maxlength="5" name="plz"
+                    <input type="text" id="inputPLZ" size="5" maxlength="5" name="postleitzahl"
                     class="form-control">
                 </div>
                         
@@ -163,13 +192,11 @@ include("templates/header.php")
             </form>
 
             <?php
-        } //Ende von if($showFormular)
+       
 
       
     ?>
 </div>
 <?php
 include("templates/footer.php");
-?>
-
 ?>
