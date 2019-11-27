ba69e4a88ab2ecf6aa554017c3a90b7cb3e32e86
Karsten König <KKoenig@it-ab.de>
2019-11-27 15:01:02 +0100

    wtf

--- deleteReservation.php
+++ deleteReservation.php
@@ -1,24 +1,37 @@
 <?php
 session_start();
+
 require_once("inc/config.php.inc");
 require_once("inc/functions.php");
+require_once('templates/header.php');
 
-require_once ('templates/header.php');
+//Überprüfe, dass der User eingeloggt ist
+//Der Aufruf von check_user() muss in alle internen Seiten eingebaut sein
 
-spl_autoload_register();
-
-use mapper\ReservierungsDAO;
-use model\Reservierung;
-
+$user = check_user();
+             
+    if (isset($_GET['deleteReservation'])) {
+        $statement = $pdo->prepare("DELETE FROM reservierung WHERE ReservierungNr = ?");
+        $statement->bindParam(1,$_GET['deleteReservation']); 
+        $result = $statement->execute(); 
+        if (!$result) {
+            ?> 
+            <script>alert("Diese Reservierung kann nicht gelöscht werden.");</script>
+            <?php
+        }             
+    }
+                              
 ?>
+
 <div class="mainContainer">
 
-        <h1>Reservierung</h1>
+        <h1>Reservierung löschen</h1>
 
         Hallo <?php echo htmlentities($user['vorname']); ?>,<br>
         Herzlich Willkommen im Reservierungsbereich!<br><br>
 
     <div class="panel panel-default">
+
 
         <table class="table">
 
@@ -30,12 +43,12 @@ use model\Reservierung;
                 <th>Datum von |</th>
                 <th>Datum bis </th>
             </tr>
-            
+
             <?php
             $statement = $pdo->prepare("SELECT * FROM reservierung ORDER BY ReservierungNr");
             $result = $statement->execute();
             $count = 1;
-            
+           
             while ($row = $statement->fetch()) {
                 echo "<tr>";
                 echo "<td>" . $row['ReservierungNr'] . "</td>";
@@ -45,17 +58,19 @@ use model\Reservierung;
                 echo "<td>" . $row['DatumVon'] . "</td>";
                 echo "<td>" . $row['DatumBis'] . "</td>";
                 echo "</tr>";
+                
+                echo '<td>
+                
+                <br><p><a class="btn btn-primary btn-lg" href="deleteReservation.php?deleteReservation=' . $row['ReservierungNr'] . '" role="button">löschen</a></p>
+               
+                </td>';
+                echo "</tr>"; 
             }
             ?>
+           
+            
+                   
         </table>
+        
     </div>
-
-    <p><a class="btn btn-primary btn-lg" href="changeReservation.php" role="button">Reservierung ändern</a></p>
-    <br><p><a class="btn btn-primary btn-lg" href="deleteReservation.php" role="button">Reservierung löschen</a></p>
-
-
 </div>
-
-<?php
-include("templates/footer.php")
-?>
